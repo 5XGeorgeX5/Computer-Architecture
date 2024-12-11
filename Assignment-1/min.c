228ba6e2a8d07fa95f135fcbcd7e3a94056556f5
George Raafat <georgexraafat@gmail.com>
2024-12-11 20:28:01 +0200

    fix array size

--- Assignment-1/min.c
+++ Assignment-1/min.c
@@ -3,7 +3,7 @@
 int main()
 {
     int array[] = {10, 31, 5, 7, 11, 3, 8, 40, 12, 4};
-    int size = sizeof(array) / sizeof(array[0]);
+    int size = 10;
     int min = array[0];
 
     for (int i = 1; i < size; i++)
